f29b8114de00c51d5a387e0bdad47b7c10a2bf47
lim <gylim@student.42seoul.kr>
2024-07-27 13:51:02 +0900

    delete photo app

--- apps/backend/srcs/config/urls.py
+++ apps/backend/srcs/config/urls.py
@@ -20,7 +20,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("photo/", include("photo.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
